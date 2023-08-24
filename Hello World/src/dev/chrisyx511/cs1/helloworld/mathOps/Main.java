d5e6b1f46e9561ae146c7dd939fc83ca032734a7
Chris Yang <chrisyx511@gmail.com>
2023-08-24 14:35:27 -0400

    Initial Commit

---
+++ Hello World/src/dev/chrisyx511/cs1/helloworld/mathOps/Main.java
@@ -0,0 +1,25 @@
+package dev.chrisyx511.cs1.helloworld.mathOps;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Opt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.print("Hello and welcome!");
+        String helloWorld = "Hello World!";
+        int test = 6;
+        char[] test2 = "asd".toCharArray();
+        System.out.println(test2);
+        int[] arrayOfNumbers = {3,4,5};
+
+
+        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 5; i++) {
+
+            // Press Ctrl+D to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Cmd+F8.
+            System.out.println("i = " + i);
+        }
+    }
+}
---
+++ Hello World/src/dev/chrisyx511/cs1/helloworld/mathOps/test.java
@@ -0,0 +1,4 @@
+package dev.chrisyx511.cs1.helloworld.mathOps;
+
+public class test {
+}
