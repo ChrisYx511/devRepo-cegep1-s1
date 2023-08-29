cec0afd689c91139193e5f14ae593fae51852f24
Chris Yang <chrisyx511@gmail.com>
2023-08-29 17:42:20 -0400

    Introduced final var, or constants

--- DataTypes/src/dev/chrisyx511/cs1/printnumbers/Main.java
+++ DataTypes/src/dev/chrisyx511/cs1/printnumbers/Main.java
@@ -3,6 +3,8 @@ package dev.chrisyx511.cs1.printnumbers;
 public class Main {
     public static void main(String[] args) {
         int number = 0;
+        final double pi = Math.PI;
+        System.out.println(pi);
         System.out.println(number);
         number += 1;
         System.out.println(number);
@@ -28,6 +30,7 @@ public class Main {
         for (int i = 0; i <= 10; i++) {
             System.out.println(i);
         }
-
+        final int constant = 4;
+        System.out.println(constant);
     }
 }
