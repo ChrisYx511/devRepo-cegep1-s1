cd0da940a302bc4514656a3d9909aec0283b50c7
Chris Yang <chrisyx511@gmail.com>
2023-08-29 16:44:15 -0400

    Aug 29 Initial Commit

---
+++ DataTypes/src/dev/chrisyx511/cs1/datatypes/Main.java
@@ -0,0 +1,22 @@
+package dev.chrisyx511.cs1.datatypes;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 99;
+        char b = 'z';
+        float c = 5.5f;
+        double d = 6.678;
+        boolean e = true;
+        byte f = 127;
+        short g = 45;
+        long h = 345;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+}
